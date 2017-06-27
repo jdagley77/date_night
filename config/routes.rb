3fa7cc40e507d0331de27382f15569ef317590b7
Jack Dagley <jack.dagley77@gmail.com>
2017-06-27 16:37:39 -0700

    Added sign up form, set up routes, created user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 
   get '/', to: 'restaurants#index'
-  get 'registration', to: 'users#new', as: 'user'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login', to: 'sessions#new'
 end
