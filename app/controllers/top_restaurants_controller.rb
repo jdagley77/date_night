88b347bbe3bcd003a4845cda83b150d7045ff28b
Jack Dagley <jack.dagley77@gmail.com>
2017-06-27 14:43:47 -0700

    Moved associations from controller to model

--- app/controllers/top_restaurants_controller.rb
+++ app/controllers/top_restaurants_controller.rb
@@ -1,4 +1,3 @@
 class TopRestaurantsController < ApplicationController
-	belongs_to :user
-	belongs_to :restaurant
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+	has_many :toprestaurants
+	has_many :users, through: :toprestaurants
+	
 end
