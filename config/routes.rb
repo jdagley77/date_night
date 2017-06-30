8a9302f5eb2868b53d015e39327135d30ccd89a4
Jack Dagley <jack.dagley77@gmail.com>
2017-06-30 15:54:37 -0700

    Add routes for top restaurants and changed syntax in model associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users do 
+  	resources :top_restaurants, only: [:new, :create, :destroy]
+  end
 
   # get '/', to: 'restaurants#index'
   root to: 'restaurants#index'
