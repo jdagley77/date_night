31ceb926b3846f86b018adba1641286d3f42ad9d
Jack Dagley <jack.dagley77@gmail.com>
2017-06-30 15:58:41 -0700

    Add routes for logging in

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   # get '/', to: 'restaurants#index'
   root to: 'restaurants#index'
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
-  get '/login', to: 'sessions#new'
+  get    '/signup', to: 'users#new'
+  post   '/users', to: 'users#create'
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
