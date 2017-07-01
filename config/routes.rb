35a82f217e1c493502cd5f698864f374091bcd53
Jack Dagley <jack.dagley77@gmail.com>
2017-07-01 12:20:37 -0700

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :sessions, only: [:new, :create, :destroy]
-
   resources :users
 
   # get '/', to: 'restaurants#index'
