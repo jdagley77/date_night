be2d2f121b7b9b5d8fc51138fd3657ab6711d046
Jack Dagley <jack.dagley77@gmail.com>
2017-07-01 11:02:55 -0700

    Add sessions routes for new, create, and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :users
 
   # get '/', to: 'restaurants#index'
