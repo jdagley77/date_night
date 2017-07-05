c48bebd487542c8eeca6bff11fac7ee534ddcbd6
Jack Dagley <jack.dagley77@gmail.com>
2017-07-05 12:53:57 -0700

    Add link to profile on index page

--- app/controllers/top_restaurants_controller.rb
+++ app/controllers/top_restaurants_controller.rb
@@ -5,7 +5,6 @@ class TopRestaurantsController < ApplicationController
 	end
 
 	def create
-		# @user = User.find(params[:user_id])
 		@restaurant = Restaurant.find(params[:restaurant_id])
 	    @toprestaurant = TopRestaurant.create(user_id: params[:user_id], restaurant_id: params[:restaurant_id])
 	    redirect_to root_path
