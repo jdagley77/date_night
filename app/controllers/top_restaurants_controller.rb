bebf93e2f6246c49cbe4a1e8f00df28b41791cee
Jack Dagley <jack.dagley77@gmail.com>
2017-07-05 18:00:33 -0700

    Attempted to fix add and delete buttons

--- app/controllers/top_restaurants_controller.rb
+++ app/controllers/top_restaurants_controller.rb
@@ -9,4 +9,14 @@ class TopRestaurantsController < ApplicationController
 	    @toprestaurant = TopRestaurant.create(user_id: params[:user_id], restaurant_id: params[:restaurant_id])
 	    redirect_to root_path
 	end
+
+	def destroy
+	    # user = User.find(params[:user_id])
+	    # restaurant = user.restaurants.find(params[:restaurant_id])
+	    # top_restaurant = TopRestaurant.find(user_id: params[:user_id], restaurant_id: params[:restaurant_id])
+	    # user.restaurants.delete(restaurant) if restaurant
+	    # redirect_to root_path
+	end
 end
+
+
